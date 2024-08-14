1f88327593b8e8b002479162cd26a5b3d3dcaa07
mallexibra <mallexibra@gmail.com>
2024-08-14 13:28:36 +0700

    fix: sidebar

--- src/components/sidebar/SidebarAdmin.tsx
+++ src/components/sidebar/SidebarAdmin.tsx
@@ -1,8 +1,15 @@
+'use client';
+
 import Image from 'next/image';
 import LogoBanyuwangi from '../../assets/logo-banyuwangi.png';
 import { cn } from '@/utils/classname';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function SidebarAdmin() {
+  const pathname = usePathname();
+  const path = pathname.split('/')[2];
+
   return (
     <nav className="min-w-max min-h-screen bg-white border-custom border-r-2">
       <div className="p-5 flex justify-center gap-2 border-custom border-b-2">
@@ -11,56 +18,67 @@ export default function SidebarAdmin() {
           DESA <br /> KARANGBENDO
         </p>
       </div>
-      <ul className="py-5 pl-5 space-y-3">
-        <li
+      <div className="py-5 pl-5 space-y-5">
+        <Link
+          href={'/admin'}
           className={cn(
-            'text-primary font-semibold w-full',
-            'border-r-2 py-2 border-primary',
+            'text-primary block outline-none font-semibold w-full',
+            path == null && 'border-r-2 py-2 border-primary',
           )}
         >
           Dashboard
-        </li>
-        <li
+        </Link>
+        <Link
+          href={'/admin/profile-desa'}
           className={cn(
-            'font-semibold w-full',
-            // 'text-primary border-r-2 py-2 border-primary',
+            'font-semibold block outline-none w-full',
+            path == 'profile-desa' &&
+              'text-primary border-r-2 py-2 border-primary',
           )}
         >
-          Profile Desa
-        </li>
-        <li
+          Profile Desa {path}
+        </Link>
+        <Link
+          href={'/admin/informasi-desa'}
           className={cn(
-            'font-semibold w-full',
-            // 'text-primary border-r-2 py-2 border-primary',
+            'font-semibold block outline-none w-full',
+            path == 'informasi-desa' &&
+              'text-primary border-r-2 py-2 border-primary',
           )}
         >
           Informasi Desa
-        </li>
-        <li
+        </Link>
+        <Link
+          href={'/admin/transparansi'}
           className={cn(
-            'font-semibold w-full',
-            // 'text-primary border-r-2 py-2 border-primary',
+            'font-semibold block outline-none w-full',
+            path == 'transparansi' &&
+              'text-primary border-r-2 py-2 border-primary',
           )}
         >
           Transparansi
-        </li>
-        <li
+        </Link>
+        <Link
+          href={'/admin/produk-hukum'}
           className={cn(
-            'font-semibold w-full',
-            // 'text-primary border-r-2 py-2 border-primary',
+            'font-semibold block outline-none w-full',
+            path == 'produk-hukum' &&
+              'text-primary border-r-2 py-2 border-primary',
           )}
         >
           Produk Hukum
-        </li>
-        <li
+        </Link>
+        <Link
+          href={'/admin/produk-umkm'}
           className={cn(
-            'font-semibold w-full',
-            // 'text-primary border-r-2 py-2 border-primary',
+            'font-semibold block outline-none w-full',
+            path == 'produk-umkm' &&
+              'text-primary border-r-2 py-2 border-primary',
           )}
         >
           Produk UMKM
-        </li>
-      </ul>
+        </Link>
+      </div>
     </nav>
   );
 }
