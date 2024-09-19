9023f1fff14d7c102564b57fcfbc4ec8967f6ab1
mallexibra <mallexibra@gmail.com>
2024-09-19 19:06:14 +0700

    chore: connect api informasi desa page

--- src/app/(client)/informasi-desa/_section/Kelembagaan.tsx
+++ src/app/(client)/informasi-desa/_section/Kelembagaan.tsx
@@ -1,32 +1,37 @@
+"use client"
 import ContainerClient from "@/components/containers/ContainerClient";
+import { VillageInstitution } from "@/types/VillageInstitution";
+import axiosInstance from "@/utils/axiosInstance";
+import { useEffect, useState } from "react";
 
 export default function Kelembagaan() {
+    const [institutions, setInstitutions] = useState<VillageInstitution[]>([])
+
+    const fetchData = async () => {
+        try {
+            const response = (await axiosInstance.get('/village-institutions')).data
+            setInstitutions(response.data);
+        } catch (error: any) {
+            console.log(`Error fetch data village institutions: ${error.message}`)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
     return (
         <ContainerClient>
             <div id="kelembagaan" className="pt-24">
                 <h2 className="title">Kelembagaan</h2>
                 <p>Desa ini memiliki beberapa kelembagaan aktif seperti kelompok tani, karang taruna, dan koperasi desa yang berperan dalam pemberdayaan masyarakat.</p>
                 <div className="flex flex-wrap justify-center gap-3 mt-3">
-                    <div className="font-medium p-3 rounded-md border-2 w-max border-custom shadow-sm">
-                        <p>Badan Permusyawaratan Desa (BPD)</p>
-                    </div>
-                    <div className="font-medium p-3 rounded-md border-2 w-max border-custom shadow-sm">
-                        <p>Karang Taruna (KARTAR)</p>
-                    </div>
-                    <div className="font-medium p-3 rounded-md border-2 w-max border-custom shadow-sm">
-                        <p>Rukun Tetangga, Rukun Warga</p>
-                    </div>
-                    <div className="font-medium p-3 rounded-md border-2 w-max border-custom shadow-sm">
-                        <p>Lembaga Pemberdayaan Masyarakat Desa</p>
-                    </div>
-                    <div className="font-medium p-3 rounded-md border-2 w-max border-custom shadow-sm">
-                        <p>Pembinaan Kesejahteraan Keluarga</p>
-                    </div>
-                    <div className="font-medium p-3 rounded-md border-2 w-max border-custom shadow-sm">
-                        <p>Kader Pemberdayaan Masyarakat Desa</p>
-                    </div>
+                    {institutions.length > 0 ? institutions.map((item: VillageInstitution, i: number) => (
+                        <div key={i} className="font-medium p-3 rounded-md border-2 w-max border-custom shadow-sm">
+                            <p>{item.name}</p>
+                        </div>
+                    )) : (<p>Data lembaga desa sedang kosong!</p>)}
                 </div>
             </div>
-        </ContainerClient>
+        </ContainerClient >
     )
 }
--- src/app/(client)/informasi-desa/_section/PotensiDesa.tsx
+++ src/app/(client)/informasi-desa/_section/PotensiDesa.tsx
@@ -1,17 +1,34 @@
+"use client"
 import ContainerClient from "@/components/containers/ContainerClient";
-import React from "react";
+import { villagePotential } from "@/types/VillagePotential";
+import axiosInstance from "@/utils/axiosInstance";
+import React, { useEffect, useState } from "react";
 
 const PotensiDesa = () => {
+    const [potential, setPotential] = useState<villagePotential[]>([])
+
+    const fetchData = async () => {
+        try {
+            const response = (await axiosInstance.get('/village-potential')).data
+            setPotential(response.data);
+        } catch (error: any) {
+            console.log(`Error fetch data village potential: ${error.message}`);
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
     return (
         <ContainerClient>
             <div>
                 <h2 className="title">Potensi Desa</h2>
                 <p>Desa Karangbendo memiliki potensi alam yang melimpah, termasuk lahan pertanian yang subur, hutan bambu, dan sungai yang kaya akan ikan.</p>
-                <div className="mt-5 flex flex-wrap justify-center gap-5">
-                    {[1, 2, 3, 4, 5].map((item: any, i: number) => (<div key={i} className="space-y-2">
-                        <img src="https://images.unsplash.com/photo-1725714834280-0c7584637d06?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D" className="rounded-md w-44" alt="Potensi Desa" />
-                        <p className="text-center font-semibold">Lahan Pertanian</p>
-                    </div>))}
+                <div className="mt-5 flex flex-wrap justify-start gap-5">
+                    {potential.length >= 1 ? potential.map((item: villagePotential, i: number) => (<div key={i} className="space-y-2">
+                        <img src={`/assets/village-potential/${item.image}`} alt={item.name}/>
+                        <p className="text-center font-semibold">{item.name}</p>
+                    </div>)) : (<p>Data potensi desa sedang kosong!</p>)}
                 </div>
             </div>
         </ContainerClient>
