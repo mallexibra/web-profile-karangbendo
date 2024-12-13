faf33237e7f778ceb0fb43cf8b20ae64d4acd7e0
mallexibra <mallexibra@gmail.com>
2024-12-13 08:26:19 +0700

    fix: build project

--- src/app/admin/_section/CheckSession.tsx
+++ src/app/admin/_section/CheckSession.tsx
@@ -1,11 +1,24 @@
 'use client';
+
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import Swal from 'sweetalert2';
 
 const CheckSession = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
+
+  useEffect(() => {
+    if (status === 'authenticated' && !session) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Gagal!',
+        text: 'Anda tidak memiliki akses',
+      });
+      router.push('/auth/login');
+    }
+  }, [status, session, router]);
 
   if (status === 'loading') {
     return (
@@ -13,15 +26,6 @@ const CheckSession = () => {
         <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-primary border-solid"></div>
       </div>
     );
-  }
-
-  if (!session) {
-    Swal.fire({
-      icon: 'error',
-      title: 'Gagal!',
-      text: 'Anda tidak memiliki akses',
-    });
-    return router.push('/auth/login');
   }
 
   return null;
